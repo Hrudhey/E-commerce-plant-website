f7e8c4790be7921b3d41d8e8b8629346faf30b0d
Hrudhey <hrudhey@gmail.com>
2026-01-05 14:55:35 +0530

    url changes

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,87 +1,92 @@
-import { createContext, useEffect, useState } from "react";
-import axios  from "axios";
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
-
-
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+  const url = 'http://localhost:5000';
+  const [token, setToken] = useState('');
+  const [plant_list, setPlantList] = useState([]);
 
-    const [cartItems, setCartItems] = useState({});
-    const [token,setToken]= useState('');
-    const [plant_list,setPlantList]= useState([])
-
-    const addToCart = async (itemId) => {
-        if (!cartItems[itemId]) {
-            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
-        }
-        else {
-            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
-        }
-       if(token){
-        await axios.post('/api/cart/add',{itemId},{headers:{token}})
-       }
+  const addToCart = async (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
-
-    const removeFromCart = async (itemId) => {
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-        if(token){
-            await axios.post('/api/cart/remove',{itemId},{headers:{token}})
-        }
+    if (token) {
+      await axios.post(
+        url + '/api/cart/add',
+        { itemId },
+        { headers: { token } }
+      );
     }
+  };
 
-    const getTotalCartAmount = () => {
-        let totalAmount = 0;
-        for (const item in cartItems) {
-            if (cartItems[item] > 0) {
-                let itemInfo = plant_list.find((product) => product._id === item);
-                totalAmount += itemInfo.price * cartItems[item];
-            }
-
-        }
-        return totalAmount;
+  const removeFromCart = async (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (token) {
+      await axios.post(
+        url + '/api/cart/remove',
+        { itemId },
+        { headers: { token } }
+      );
     }
+  };
 
-    const fetchPlantList= async()=>{
-        const response= await axios.get('/api/plant/list')
-        setPlantList(response.data.data)
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = plant_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
     }
+    return totalAmount;
+  };
 
-    const loadCartData= async(token)=>{
-        const response= await axios.post('/api/cart/get',{},{headers:{token}});
-        setCartItems(response.data.cartData);
+  const fetchPlantList = async () => {
+    const response = await axios.get(url + '/api/plant/list');
+    setPlantList(response.data.data);
+  };
+
+  const loadCartData = async (token) => {
+    const response = await axios.post(
+      url + '/api/cart/get',
+      {},
+      { headers: { token } }
+    );
+    setCartItems(response.data.cartData);
+  };
+
+  useEffect(() => {
+    async function loadData() {
+      await fetchPlantList();
+      if (localStorage.getItem('token')) {
+        setToken(localStorage.getItem('token'));
+        await loadCartData(localStorage.getItem('token'));
+      }
     }
+    loadData();
+  }, []);
 
-    useEffect(()=>{
-       
-       async function loadData(){
-        await fetchPlantList();
-        if(localStorage.getItem('token')){
-            setToken(localStorage.getItem('token'));
-            await loadCartData(localStorage.getItem('token'));
-           }
-       }
-       loadData();
-    },[])
-
-    const contextValue = {
-        plant_list,
-        cartItems,
-        setCartItems,
-        addToCart,
-        removeFromCart,
-        getTotalCartAmount,
-        url,
-        token,
-        setToken
-    }
-    return (
-        <StoreContext.Provider value={contextValue}>
-            {props.children}
-        </StoreContext.Provider>
-    )
-}
-
-
+  const contextValue = {
+    plant_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount,
+    url,
+    token,
+    setToken,
+  };
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
 
 export default StoreContextProvider;
